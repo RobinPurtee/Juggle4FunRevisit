feff949e044ef093eda13a55e4bfb751a6ecf8b6
Rick Purtee <sourcetree@purtee.us>
2017-07-06 20:32:51 -0700

    Experimental stage of the pleat object and such

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,5 +1,5 @@
 
-function foldClickHandler(){
+function foldClickHandler(mouseEvt){
     /* Toggle between adding and removing the "active" class, to highlight the button that controls the panel */
     this.classList.toggle("active");
 
@@ -14,26 +14,17 @@ function foldClickHandler(){
 
 class Pleat{
 
-    toggleFold(buttonEvt){ 
-        /* Toggle between adding and removing the "active" class, to highlight the button that controls the panel */
-        this.classList.toggle("active");
-
-        /* Toggle between hiding and showing the active panel */
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }   
-    }
-
-
     constructor(element){
         this.article = element;
+        this.article.style.display = "none";
+        this.article.onclick = this.contract;
         let title = element.getElementsByTagName("h1");
         if(0 < title.length){
             this.fold = document.createElement("Button");
+            this.fold.id = this.article.id + "-button";
+            this.fold.onclick = this.expand;
             this.fold.onclick =  foldClickHandler;
+            this.fold.style.display = "block";
 
             this.fold.className = "pleatButton";    
             this.fold.innerHTML = title[0].innerHTML;
@@ -41,9 +32,36 @@ class Pleat{
         }
     }
 
+    expand(){
+        this.fold.toggle("active", true);
+        this.article.style.display = "block";
+    }
+
+    contract(){
+        this.fold.toggle("active", false);
+        this.article.style.display = "none";
+    }
+
 }
 
+class Pleats{
+    
+    constructor(articles)
+    {
+        var pleats = new Array();
+        if(0 < articles.length){
+            for(let i = 0; i < articles.length ; ++i){
+                logArticle(articles[i]);
+                let curPleat = new Pleat(articles[i]);
+                curPleat.fold.onclick = curPleat.expand;
+                
+                curPleat.article.onclick = curPleat.contract; 
+                pleats.push(curPleat);
+            }
+        }
+    }
 
+}
 
 function logArticle(element){
     console.log(element.id);
@@ -54,16 +72,10 @@ function logArticle(element){
 }
 
 
-var pleats = new Array();
 
 function listArticles(){
     let articles = document.getElementsByTagName("article");
-    if(0 < articles.length){
-        for(let i = 0; i < articles.length ; ++i){
-            logArticle(articles[i]);
-            pleats.push(new Pleat(articles[i]));
-        }
-    }
+    pleats = new Pleats(articles);
 }
 
 
