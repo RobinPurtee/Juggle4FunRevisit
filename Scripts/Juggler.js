ab4a1ce250f0fba0716729aa67eb9ed52ebc2c49
Rick Purtee <sourcetree@purtee.us>
2017-08-04 11:41:33 -0700

    Added Toss object and testing for it using QUnit, forced a move from Tests folder to Test folder

--- Scripts/Juggler.js
+++ Scripts/Juggler.js
@@ -1,27 +1,42 @@
+// hand strings
 var RightHand = "R";
 var LeftHand = "L";
+// Toss direction strings
 var Pass = "P";
 var Diagonal = "D";
 var Self = "S";
 var Heff = "H";
 
-
+// The object that represents a Toss 
+// parameters:
+//      direction_ - A Toss direction string (maybe a full Toss string)
+//      magnitude_ - This siteswap value of the pass (optional: defaults to 3)
+//      juggler_   - The name of the destination juggler (optional: defaults to "A")
 
 function Toss(direction_, magnitude_, juggler_) {
     this.originJuggler = null;
     this.originHand = RightHand;
-    this.magnitude = 3;
-    this.direction = Pass;
-    this.juggler = null;
+    this.magnitude = magnitude_;
+    this.direction = direction_;
+    this.juggler = juggler_;
     this.tossPath = null;
-    if (!(magnitude_ instanceof undefined) && !isNaN(magnitude_)) {
-        this.magnitude = magnitude_;
-    }
-    if (direction_ instanceof String) {
-        this.direction = direction_;
-    }
-    if (juggler_ instanceof String) {
-        this.juggler = juggler_;
+
+    if (direction_ != undefined && direction_.length > 1) {
+
+    } else {
+        if (direction_ === undefined) {
+            this.direction = Self;
+        }
+
+        if (this.magnitude === undefined || !isNaN(this.magnitude)) {
+            this.magnitude = 3;
+        }
+        if (direction_ === undefined) {
+            this.direction = Self;
+        }
+        if (juggler_ === undefined) {
+            this.juggler = null;
+        }
     }
 }
 
@@ -34,8 +49,12 @@ Toss.prototype.SetDisplay = function(show) {
     this.tossPath.setAttribute("display", state);
 }
 
-Toss.prototype.Show = function() { this.SetDisplay(true); }
-Toss.prototype.Hide = function() { this.SetDisplay(false); }
+Toss.prototype.Show = function() {
+    this.SetDisplay(true);
+}
+Toss.prototype.Hide = function() {
+    this.SetDisplay(false);
+}
 
 Toss.prototype.shortDestinationString = function() {
     let retStr = new String();
@@ -48,7 +67,7 @@ Toss.prototype.shortDestinationString = function() {
 
 Toss.prototype.destinationString = function() {
     let retStr = this.shortDestinationString();
-    if (this.juggler != null)
+    if (this.juggler != null && this.direction != Self && this.direction != Heff)
         retStr += this.juggler;
     return retStr;
 }
@@ -57,7 +76,7 @@ Toss.prototype.destinationString = function() {
 Toss.prototype.toString = function() {
     let retStr = new String();
     if (this.originJuggler != null) {
-        retStr = this.originJuggler.name;
+        retStr = this.originJuggler;
         retStr += this.originHand;
         retStr += "-";
     }
@@ -66,17 +85,18 @@ Toss.prototype.toString = function() {
     if (this.juggler != null) {
         retStr += this.juggler;
     } else {
-        retStr += 'A';
+        retStr += "A";
     }
     return retStr;
 }
 
 
-
 function Juggler(name_, patternRow_) {
     this.name = name_;
     this.tossHand = RightHand;
-    this.tosses = new Array(patternRow_.cells.length);
+    if (patternRow_ != null) {
+        this.tosses = new Array(patternRow_.cells.length);
+    }
 
     this.toString = function() {
         let ret = this.name + this.tossHand;
